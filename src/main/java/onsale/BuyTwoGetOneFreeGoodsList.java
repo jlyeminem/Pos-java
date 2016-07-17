a60c46a31d670920377cdafe95d496a838c7b7b9
YT <alkohlbaby@gmail.com>
2016-07-17 16:08:37 +0800

    [YT] 增加促销商品信息        说明:        上周六的时候, 说了下促销商品,不能放到goods里面,因此,单独提出了一个包: onsale, 存放各种促销的促销商品信息

---
+++ src/main/java/onsale/BuyTwoGetOneFreeGoodsList.java
@@ -0,0 +1,19 @@
+package onsale;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//享受买二赠一促销的商品信息
+public class BuyTwoGetOneFreeGoodsList {
+    private static List<String> promotionBarcodes = new ArrayList<>();
+
+    static {
+        promotionBarcodes.add("ITEM000001");  //羽毛球
+        promotionBarcodes.add("ITEM000005");  //可口可乐
+
+    }
+
+    public List<String> getPromotionBarcodes() {
+        return promotionBarcodes;
+    }
+}
---
+++ src/main/java/onsale/NinetyFiveDiscountGoodsList.java
@@ -0,0 +1,18 @@
+package onsale;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//享受95折促销的商品信息
+public class NinetyFiveDiscountGoodsList {
+    private static List<String> promotionBarcodes = new ArrayList<>();
+
+    static {
+        promotionBarcodes.add("ITEM000003-"); //苹果
+
+    }
+
+    public List<String> getPromotionBarcodes() {
+        return promotionBarcodes;
+    }
+}
