550f8bde01d70c615cbe9e046dbe179d99177c13
YT <alkohlbaby@gmail.com>
2016-07-17 14:34:11 +0800

    [YT] 在Goods(商品信息)中添加"类别"属性

--- src/main/java/goods/Goods.java
+++ src/main/java/goods/Goods.java
@@ -8,44 +8,30 @@ public class Goods {
     private String name;
     private String unit;
     private double price;
+    private String category;
 
 
-    public Goods(String barcode, String name, String unit, double price) {
+    public Goods(String barcode, String name, String unit, double price,String category) {
         this.barcode = barcode;
         this.name = name;
         this.unit = unit;
         this.price = price;
+        this.category = category;
     }
 
     public String getBarcode() {
         return barcode;
     }
 
-    public void setBarcode(String barcode) {
-        this.barcode = barcode;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
     public String getUnit() {
         return unit;
-    }
-
-    public void setUnit(String unit) {
-        this.unit = unit;
     }
 }
--- src/main/java/goods/GoodsMap.java
+++ src/main/java/goods/GoodsMap.java
@@ -9,12 +9,13 @@ public class GoodsMap {
     private static HashMap<String, Goods> map = new HashMap<>();
 
     static {
-        Goods goods = new Goods("ITEM000005","可口可乐","瓶",3.00);
-        map.put("ITEM000005",goods);
-        goods = new Goods("ITEM000003","苹果","斤",5.50);
-        map.put("ITEM000003",goods);
-        goods = new Goods("ITEM000001","羽毛球","个",1.00);
+        Goods goods = new Goods("ITEM000001","羽毛球","个",1.00,"球类");
         map.put("ITEM000001",goods);
+        goods = new Goods("ITEM000003","苹果","斤",5.50,"水果");
+        map.put("ITEM000003",goods);
+        goods = new Goods("ITEM000005","可口可乐","瓶",3.00,"饮料");
+        map.put("ITEM000005",goods);
+
     }
 
     public static HashMap<String, Goods> getMap() {
