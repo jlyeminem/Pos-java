8f10d07d4914a13d79434c6220ce2aa0fe8d3040
YT <alkohlbaby@gmail.com>
2016-07-19 21:59:28 +0800

    [YT]  添加cart中类Cart对应的测试单元,测试通过,覆盖率通过

--- src/main/java/cart/Cart.java
+++ src/main/java/cart/Cart.java
@@ -5,6 +5,7 @@ import goods.GoodsMap;
 import promotion.Promotion;
 import utils.ParseUtils;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,7 +15,7 @@ import java.util.Map;
 //根据购物车和促销信息得到购物车中商品对应的打折类型,条形码,购买数量,实际价格,打折价格
 public class Cart {
     private Map<String,Integer> cartMap = new HashMap<>();
-    List<Item> purchasedList = new ArrayList<>();
+    private List<Item> purchasedList = new ArrayList<>();
 
     //根据购物车中的商品,将商品条形码和对应数目存放到cartMap
     public Cart(List<String> wantsToBuy) {
@@ -29,6 +30,10 @@ public class Cart {
                 cartMap.put(barcode, num);
             }
         }
+    }
+
+    public List<Item> getPurchasedList() {
+        return purchasedList;
     }
 
     //根据购物车和促销信息,将购物车中商品对应的打折类型,条形码,购买数量,实际价格,打折价格存放到purchasedList
@@ -59,15 +64,16 @@ public class Cart {
     //根据purchasedList中的商品信息进行打印,打印出用户的购买清单
     public String printShoppingList() {
         StringBuilder sb = new StringBuilder("***<没钱赚商店>购物清单***\n");
+        DecimalFormat df   = new DecimalFormat("######0.00");
         for (int i = 0;i < purchasedList.size();i++) {
             Item item = purchasedList.get(i);
             String barcode = item.getBarcode();
             Goods goods = GoodsMap.getMap().get(barcode);
             double save = item.getPrice() - item.getPromotedPrice();
             sb.append("名称:" + goods.getName() + ",数量:" + item.getNum() + goods.getUnit()
-                    + ",单价:" + goods.getPrice() + "(元),小计:" + item.getPromotedPrice() + "(元)");
+                    + ",单价:" + df.format(goods.getPrice()) + "(元),小计:" + df.format(item.getPromotedPrice()) + "(元)");
             if (save > 0 && item.getType().equals("FIVE_PERCENT_DISCOUNT")) {
-                sb.append(",节省:" + Double.parseDouble(String.format("%.2f",save)) + "(元)");
+                sb.append(",节省:" + df.format(save) + "(元)");
             }
             sb.append("\n");
         }
@@ -88,11 +94,11 @@ public class Cart {
                 if (count == 1) {
                     sb.append("满二赠一商品:\n");
                 }
-                sb.append("名称:" + goods.getName() + "数量:" + (item.getNum() / 3) + goods.getUnit());
+                sb.append("名称:" + goods.getName() + ",数量:" + (item.getNum() / 3) + goods.getUnit());
                 sb.append("\n");
             }
         }
-        if (count > 0) {
+        if (count > 0 && type.equals("BUY_TWO_GET_ONE_FREE")) {
             sb.append("----------------------\n");
         }
         return sb.toString();
@@ -100,6 +106,7 @@ public class Cart {
 
     //
     public String printPayInf() {
+        DecimalFormat df   = new DecimalFormat("######0.00");
         double totalPay = 0.00;
         double realPay = 0.00;
         StringBuilder sb = new StringBuilder();
@@ -108,10 +115,10 @@ public class Cart {
             totalPay += item.getPrice();
             realPay += item.getPromotedPrice();
         }
-        sb.append("总计:" + realPay + "(元)\n");
+        sb.append("总计:" + df.format(realPay) + "(元)\n");
         double save = totalPay - realPay;
         if (totalPay > realPay) {
-            sb.append("节省:" + Double.parseDouble(String.format("%.2f",save)) + "(元)\n");
+            sb.append("节省:" + df.format(save) + "(元)\n");
         }
         sb.append("**********************");
         return sb.toString();
